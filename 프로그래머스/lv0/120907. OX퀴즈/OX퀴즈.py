236d05e637becebc64af2a1d2e0838aeb8e71821
calaf <117057567+ca1af@users.noreply.github.com>
2022-12-09 18:48:09 +0900

    [level 0] Title: OX퀴즈, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -1,3 +1,6 @@
+def valid(equation):
+    equation = equation.replace('=', '==')
+    return eval(equation)
+
 def solution(quiz):
-    answer = []
-    return answer
+    return ["O" if valid(equation) else "X" for equation in quiz]

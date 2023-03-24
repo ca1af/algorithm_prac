4cbd8599a9639bc14f8b816368be96f1a520abf6
calaf <117057567+ca1af@users.noreply.github.com>
2023-03-25 02:32:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,19 +1,12 @@
 class Solution {
 public static int[] solution(int[] arr) {
-    // 배열의 길이가 1인 경우 예외처리
-    if (arr.length == 1) {
-        return new int[] {-1};
-    }
-    
-    // 배열에서 가장 작은 수의 인덱스를 찾음
     int minIndex = 0;
     for (int i = 1; i < arr.length; i++) {
         if (arr[i] < arr[minIndex]) {
             minIndex = i;
         }
     }
-    
-    // 가장 작은 수를 제외한 배열을 생성하여 반환
+
     int[] result = new int[arr.length - 1];
     int j = 0;
     for (int i = 0; i < arr.length; i++) {

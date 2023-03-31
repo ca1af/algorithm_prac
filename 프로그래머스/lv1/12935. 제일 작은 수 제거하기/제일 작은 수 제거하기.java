1c7dd4bee57a4c0d75bb14ab2cca21672ed4b949
calaf <117057567+ca1af@users.noreply.github.com>
2023-03-31 09:57:15 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,21 +1,21 @@
 class Solution {
-public static int[] solution(int[] arr) {
-    int minIndex = 0;
-    for (int i = 1; i < arr.length; i++) {
-        if (arr[i] < arr[minIndex]) {
-            minIndex = i;
+    public static int[] solution(int[] arr) {
+        int minIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
         }
-    }
 
-    int[] result = new int[arr.length - 1];
-    int j = 0;
-    for (int i = 0; i < arr.length; i++) {
-        if (i != minIndex) {
-            result[j] = arr[i];
-            j++;
+        int[] result = new int[arr.length - 1];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != minIndex) {
+                result[j] = arr[i];
+                j++;
+            }
         }
+
+        return result;
     }
-    
-    return result;
-}
 }

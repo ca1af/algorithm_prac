ed8ed2f7e54c4107943a488d010833691071ad67
calaf <117057567+ca1af@users.noreply.github.com>
2023-05-19 16:00:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 632 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine().trim(); // 문자열 입력 및 앞뒤 공백 제거
+        Scanner sc = new Scanner(System.in);
 
-        if (input.isEmpty()) {
-            System.out.println(0); // 입력이 공백인 경우 단어 개수는 0
-        } else {
-            String[] words = input.split("\\s+"); // 공백을 기준으로 문자열 분리
-            System.out.println(words.length); // 분리된 단어 개수 출력
+        String line = sc.nextLine().trim(); 
+
+        int count = 0;
+        if (!line.isEmpty()) {  
+            count = line.split("\\s+").length;
         }
+
+        System.out.println(count);
     }
 }

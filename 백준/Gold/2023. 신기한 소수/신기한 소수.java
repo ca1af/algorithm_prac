4805ff570f71aba2a4ea8f258965f268fc350fb1
calaf <117057567+ca1af@users.noreply.github.com>
2023-06-27 10:35:08 +0900

    [Gold V] Title: 신기한 소수, Time: 872 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -2,34 +2,37 @@ import java.util.*;
 
 public class Main {
         static int N;
+
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            N = sc.nextInt(); // 주어지는 자릿수
+            N = sc.nextInt();
+
             dfs(2, 1);
             dfs(3, 1);
             dfs(5, 1);
             dfs(7, 1);
-
         }
 
-        static void dfs(int num, int digit){ // 타겟, 자리수 // 2 : 21 / 22 ...
-            if (digit == N){
-                if (isPrime(num)){
+        static void dfs(int num, int digit){
+            if (digit == N){ // 들어온 게 자릿수고
+                if (isPrime(num)){ // 소수라면
                     System.out.println(num);
                 }
                 return;
             }
-            for (int i = 1; i < 10; i++) { // 1~9 까지, 자릿수 판별임
-                if (i % 2 == 0) continue; // 짝수라면 거름. 소수일필요는 없음. 왜냐면 마지막 자리의 수가 짝수면 무조건 2로 나눠지니까 -> 소수가아님.
-                if (isPrime(num * 10 + i)){
+
+            for (int i = 1; i < 10; i++) { // 1 ~ 9 까지의 수
+                if (i % 2 == 0) continue;
+                if (isPrime(num * 10 + i)){ // 소수에 * 10 하고, 거기에 i를 더한 값이 소수라면, 다시 dfs 해서 소수판별+자릿수판별
                     dfs(num * 10 + i, digit + 1);
                 }
             }
+
         }
 
-        static boolean isPrime(int num){
+        static boolean isPrime(int num) { // 소수판별로직
             for (int i = 2; i <= num / 2; i++) {
-                if (num % i == 0){
+                if (num % i == 0) {
                     return false;
                 }
             }

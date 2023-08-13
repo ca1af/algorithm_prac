a3246b96696b120cf36a0be7fc6fe23d7d19bc95
calaf <117057567+ca1af@users.noreply.github.com>
2023-08-13 19:28:04 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -5,28 +5,27 @@ public class Main {
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken()); // 목표 금액
 
-            int K = Integer.parseInt(st.nextToken()); // 주어지는 동전의 개수
-            int N = Integer.parseInt(st.nextToken()); // 목표금액 N
-
-            int[] kArr = new int[K];
-
-            for (int i = 0; i < K; i++) {
-                kArr[i] = Integer.parseInt(br.readLine());
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < N; i++) {
+                stack.push(Integer.parseInt(br.readLine()));
             }
 
             int count = 0;
 
-            for (int i = kArr.length - 1; i >= 0; i--) {
-                if (kArr[i] <= N){
-                    while (N >= kArr[i]){
-                        N -= kArr[i];
-                        count++;
-                    }
-                }
+            while (!stack.isEmpty()){
+                if (M == 0) break;
+
+                Integer popped = stack.pop();
+
+                if (M % popped == M) continue;
+
+                count+= M / popped;
+                M %= popped;
             }
 
             System.out.println(count);
-
         }
     }

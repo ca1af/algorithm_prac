8e4068bf3160214668bc6e8d87b83b187c22386f
calaf <117057567+ca1af@users.noreply.github.com>
2023-12-08 14:22:29 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 1736 ms, Memory: 250460 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
@@ -1,51 +1,60 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-public class Main {
+public class Main{
+        private static boolean[] isPrime;
+        private static List<Integer> answerList = new ArrayList<>();
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static boolean prime[] = new boolean[100000001];
+            int start = Integer.parseInt(st.nextToken()); // strat
+            int end = Integer.parseInt(st.nextToken()); // end
 
-    public static void primeCal(int m) {
-        for (int i = 2; i * i <= m; i++) {
-            if (!prime[i]) {
-                for (int j = i * i; j <= m; j += i) {
-                    prime[j] = true;
+            isPrime = new boolean[100_000_001];
+
+            Arrays.fill(isPrime, true);
+
+            isPrime[0] = isPrime[1] = false;
+
+            filter(end);
+
+            addToList(start, end);
+
+            for (Integer i : answerList) {
+                System.out.println(i);
+            }
+
+            System.out.println(-1);
+        }
+
+        private static void filter(int end){
+            for (int i = 2; i * i <= end; i++) {
+                if (isPrime[i]){
+                    for (int j = i * i; j <= end; j += i){
+                        isPrime[j] = false;
+                    }
                 }
             }
         }
-    }
 
-    public static boolean isPal(int num) {
-        String number = String.valueOf(num);
-        String reverseNumber = "";
-
-        for (int i = number.length() - 1; i >= 0; i--) {
-            reverseNumber += number.charAt(i);
-        }
-
-        if (reverseNumber.equals(number)) {
-            return true;
-        }
-
-        return false;
-    }
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        StringBuilder sb = new StringBuilder();
-
-        primeCal(m);
-
-        for (int i = n; i <= m; i++) {
-            if (prime[i] == false && isPal(i) == true) {
-                sb.append(i).append("\n");
+        public static void addToList(int start, int end){
+            for (int i = start; i <= end; i++) {
+                if (isPrime[i] && isPalindrome(i)){
+                    answerList.add(i);
+                }
             }
         }
 
-        sb.append(-1);
-        System.out.println(sb);
+        private static boolean isPalindrome(int i){
+            String string = String.valueOf(i);
+            for (int j = 0; j < string.length() / 2; j++) {
+                if (string.charAt(j) != string.charAt(string.length() - j - 1)){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-}
